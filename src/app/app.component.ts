9246d93c36be2e5c8b985515bfc5a477a45acb3a
udayk <udaykumar1113@gmail.com>
2021-01-31 16:13:17 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: String;
+  date: String;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make : 'HONDA',
+    model : 'ACCORD',
+    year : '2008'
+  }
+  
+  onMilesChange(value: string){
+    this.miles = parseFloat(value);
+  }
+
+  onNameChange(value: string){
+    this.name = value;
+  }
+
+  onDateChange(value: string){
+    this.date = value;
+  }
+
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value:string){
+    this.height = parseFloat(value);
+  }
 }
